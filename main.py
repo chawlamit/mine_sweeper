63d9a7c3ea8dc68e0cf65afb0c710eaf9d7dda1d
kartk <kaarthikgovindappa@gmail.com>
2019-10-22 23:16:12 -0400

    Added Probability Agent

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from agents.final_agent import Agent
 from agents.probabilistic_agent import ProbabilisticAgent
 from agents.mine_aware_agent import MineAwareAgent
 from agents.csp_Agent import CSPAgent
-
+from agents.probability_agent import ProbabilityAgent
 import time
 
-env = Environment(30, 180)
+env = Environment(10, 20)
 env.show()
 
 scores = []
@@ -16,22 +16,26 @@ scores = []
 # agent.run()
 # scores.append(agent.calc_score())
 #
-agent = CSPAgent(env, visualize=False, debug=False)
-st = time.time()
-agent.run()
-print('time: ', agent.__class__.__name__, time.time() - st)
-scores.append(agent.calc_score())
-#
-agent = MineAwareAgent(env, visualize=False)
-st = time.time()
-agent.run()
-print('time: ', agent.__class__.__name__, time.time() - st)
-scores.append(agent.calc_score())
-#
-agent = Agent(env, visualize=False, debug=False)
-st = time.time()
-agent.run()
-print('time: ', agent.__class__.__name__, time.time() - st)
-scores.append(agent.calc_score())
+# agent = CSPAgent(env, visualize=True, debug=True)
+# st = time.time()
+# agent.run()
+# print('time: ', agent.__class__.__name__, time.time() - st)
+# scores.append(agent.calc_score())
+# #
+# agent = MineAwareAgent(env, visualize=False)
+# st = time.time()
+# agent.run()
+# print('time: ', agent.__class__.__name__, time.time() - st)
+# scores.append(agent.calc_score())
+# #
+# agent = Agent(env, visualize=False, debug=False)
+# st = time.time()
+# agent.run()
+# print('time: ', agent.__class__.__name__, time.time() - st)
+# scores.append(agent.calc_score())
+
+agent2 = ProbabilityAgent(env,visualize=True,debug=True)
+agent2.run()
+scores.append(agent2.calc_score())
 
 print(scores)
